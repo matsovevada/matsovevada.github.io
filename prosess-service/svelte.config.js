2cc8490795d5d0b0d38bfdb9a1105316285d9841
matsovevada <matsovevada@hotmail.com>
2023-06-26 12:51:37 +0200

    changes to layout.svelte

--- prosess-service/svelte.config.js
+++ prosess-service/svelte.config.js
@@ -5,7 +5,15 @@ const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        }),
         paths: {
             base: dev ? '' : process.env.BASE_PATH,
         }
