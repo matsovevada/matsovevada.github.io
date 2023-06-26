8a4d2360507860c8336bb30ecc81015e7efa8fae
matsovevada <matsovevada@hotmail.com>
2023-06-26 13:04:53 +0200

    testing workflow

--- prosess-service/svelte.config.js
+++ prosess-service/svelte.config.js
@@ -8,8 +8,8 @@ const config = {
         adapter: adapter({
             // default options are shown. On some platforms
             // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
+            pages: 'prosess-service/build',
+            assets: 'prosess-service/build',
             fallback: undefined,
             precompress: false,
             strict: true
