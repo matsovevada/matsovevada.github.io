069ccf9d92582bf20a10d1084d6c2b015d1f11a8
matsovevada <matsovevada@hotmail.com>
2023-06-26 12:08:38 +0200

    Adding deploy workflow

---
+++ prosess-service/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<div>
+	<header>Hello world!</header>
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
